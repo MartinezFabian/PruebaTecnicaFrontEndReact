e353b47a91c3687da45d30dc613182e60f08353d
MartinezFabian <FabianMartinezDev@gmail.com>
2024-05-06 20:10:28 -0300

    Feat: Add initial layout to RegisterPage.jsx

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,7 +1,182 @@
+import { Visibility, VisibilityOff } from '@mui/icons-material';
+import {
+  Button,
+  FormControl,
+  Grid,
+  IconButton,
+  InputAdornment,
+  InputLabel,
+  OutlinedInput,
+  Paper,
+  TextField,
+  Typography,
+} from '@mui/material';
+import { useState } from 'react';
+
 export const RegisterPage = () => {
+  // show / hide password functionality
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+  const [showRepeatPassword, setShowRepeatPassword] = useState(false);
+
+  const handleClickShowRepeatPassword = () => setShowRepeatPassword((show) => !show);
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
   return (
-    <>
-      <h1>RegisterPage</h1>
-    </>
+    <Grid
+      backgroundColor={'primary.main'}
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      sx={{ minHeight: '100vh', padding: 4 }}
+    >
+      <Paper
+        sx={{
+          boxShadow:
+            'rgba(0, 0, 0, 0.1) 0px 20px 25px -5px, rgba(0, 0, 0, 0.04) 0px 10px 10px -5px;',
+          paddingTop: 6,
+          paddingBottom: 6,
+          paddingLeft: 4,
+          paddingRight: 4,
+          borderRadius: 3,
+          width: { sm: 400, md: 500 },
+        }}
+      >
+        <Grid container direction="column" alignItems="center" justifyContent="center" spacing={0}>
+          <Typography
+            component="h1"
+            textTransform={'uppercase'}
+            align="center"
+            fontWeight={'bold'}
+            variant="h5"
+            sx={{ marginBottom: 2 }}
+          >
+            Sistema de Pacientes
+          </Typography>
+
+          <Typography component="h3" align="center" variant="h5" sx={{ marginBottom: 2 }}>
+            Registro
+          </Typography>
+        </Grid>
+
+        <form>
+          <Grid container>
+            <Grid item xs={12} sx={{ marginTop: 2 }}>
+              <TextField
+                label="Nombre"
+                type="text"
+                variant="outlined"
+                fullWidth
+                placeholder="Nombre"
+                name="name"
+              />
+            </Grid>
+
+            <Grid item xs={12} sx={{ marginTop: 2 }}>
+              <TextField
+                label="Apellido"
+                type="text"
+                variant="outlined"
+                fullWidth
+                placeholder="Apellido"
+                name="lastname"
+              />
+            </Grid>
+
+            <Grid item xs={12} sx={{ marginTop: 2 }}>
+              <TextField
+                label="Nombre de usuario"
+                type="text"
+                autoComplete="username"
+                variant="outlined"
+                fullWidth
+                placeholder="Nombre de usuario"
+                name="username"
+              />
+            </Grid>
+
+            <Grid item xs={12} sx={{ marginTop: 2 }}>
+              <FormControl fullWidth variant="outlined">
+                <InputLabel htmlFor="outlined-adornment-password">Contraseña</InputLabel>
+                <OutlinedInput
+                  label="Contraseña"
+                  placeholder="Contraseña"
+                  id="outlined-adornment-password"
+                  variant="outlined"
+                  autoComplete="current-password"
+                  type={showPassword ? 'text' : 'password'}
+                  name="password"
+                  inputProps={{
+                    'aria-label': 'password-input',
+                  }}
+                  endAdornment={
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={handleClickShowPassword}
+                        onMouseDown={handleMouseDownPassword}
+                        edge="end"
+                      >
+                        {showPassword ? <VisibilityOff /> : <Visibility />}
+                      </IconButton>
+                    </InputAdornment>
+                  }
+                />
+              </FormControl>
+            </Grid>
+
+            <Grid item xs={12} sx={{ marginTop: 2 }}>
+              <FormControl fullWidth variant="outlined">
+                <InputLabel htmlFor="outlined-adornment-password">Repetir Contraseña</InputLabel>
+                <OutlinedInput
+                  label="Repetir Contraseña"
+                  placeholder="Repetir Contraseña"
+                  id="outlined-adornment-password"
+                  variant="outlined"
+                  autoComplete="current-password"
+                  type={showRepeatPassword ? 'text' : 'password'}
+                  name="password-repeat"
+                  inputProps={{
+                    'aria-label': 'password-input',
+                  }}
+                  endAdornment={
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={handleClickShowRepeatPassword}
+                        onMouseDown={handleMouseDownPassword}
+                        edge="end"
+                      >
+                        {showRepeatPassword ? <VisibilityOff /> : <Visibility />}
+                      </IconButton>
+                    </InputAdornment>
+                  }
+                />
+              </FormControl>
+            </Grid>
+
+            <Grid container spacing={2} sx={{ marginBottom: 2, marginTop: 2 }}>
+              <Grid item xs={12} sm={6} sx={{ order: { xs: 2, sm: 1 } }}>
+                <Button variant="contained" fullWidth>
+                  Iniciar sesión
+                </Button>
+              </Grid>
+              <Grid item xs={12} sm={6} sx={{ order: { xs: 1, sm: 2 } }}>
+                <Button type="submit" variant="contained" fullWidth>
+                  Crear cuenta
+                </Button>
+              </Grid>
+            </Grid>
+          </Grid>
+        </form>
+      </Paper>
+    </Grid>
   );
 };
